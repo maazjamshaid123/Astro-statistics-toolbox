1d8acbdf982da84a09cc467a3e574b9f37494b54
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-03-04 17:45:59 +0500

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -77,8 +77,8 @@ def show_analysis():
             elif file.name.endswith('.jpg') or file.name.endswith('.jpeg') or file.name.endswith('.png'):
                 data = io.imread(file)
                 data = px.imshow(data, width=1000, height=700)
-            st.subheader("Data:")
-            st.write(data)
+            st.subheader("Data (Editable):")
+            st.experimental_data_editor(data)
 
             if file.name.endswith('.csv') or file.name.endswith('.xlsx'):
                 plot_type = st.selectbox("Select Plot Type", ["Scatter", "Color-Color", "Line", "Light Curve", "Bar", "Horizontal Bar", "Histogram", "Density Heatmap", "PCA Analysis", "Contour Plot", "Distplot", "Residual", "Ordinary Least Square (OLS)", "Receiver Operating Characteristics (ROC)", "Enhanced Prediction Error Analysis", "3D Scatter"])
--- intro.py
+++ intro.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 def show_intro():
     col1, col2, col3 = st.columns(3)
@@ -27,8 +28,8 @@ def show_intro():
     Odyssey makes it easy for scientists to analyze large datasets, visualize complex data, 
     and develop effective solutions to a variety of problems.
 
-    AstroAlgo is a cutting-edge company that is dedicated to advancing the field of space 
-    science through innovative data-driven solutions. With a team of expert scientists and 
+    AstroAlgo is a Space Research company that is dedicated to advancing the field of space 
+    sciences through innovative data-driven solutions. With a team of expert scientists and 
     engineers, AstroAlgo is committed to providing cutting-edge tools and technologies that 
     help scientists better understand the universe around us.
     
@@ -37,3 +38,44 @@ def show_intro():
     user-friendly interface and advanced features, Odyssey can help you take your research to 
     the next level and unlock new insights into the mysteries of the cosmos.
     ''')
+
+    st.markdown("---")
+
+    st.error('Astronomical Analysis', icon = "🪐")
+    # Define the list of items to display
+    items = [
+        'Scatter Plot',
+        'Color-Color Plot',
+        'Line Plot',
+        'Plot For Light Curve',
+        'Bar Plot',
+        'Horizontal Bar Plot',
+        'Histogram',
+        'Density Heatmap',
+        'PCA Analysis',
+        'Contour Plot',
+        'Distplot',
+        'Residual',
+        'Ordinary Least Square (OLS)',
+        'Receiver Operating Characteristics (ROC)',
+        'Enhanced Prediction Error Analysis',
+        '3D Scatter'
+    ]
+
+    # Calculate the number of rows required to display all the items
+    num_rows = len(items) // 3 + 1
+
+    # Use a for loop to display each item in a separate column
+    for i in range(num_rows):
+        col1, col2, col3 = st.columns(3)
+        if i*3 < len(items):
+            col1.info(items[i*3])
+        if i*3+1 < len(items):
+            col2.info(items[i*3+1])
+        if i*3+2 < len(items):
+            col3.info(items[i*3+2])
+
+    st.markdown("---")
+
+
+
