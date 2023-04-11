b82cbb6e36b9b5b553dc805fd437402451d4650f
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-04-11 12:07:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from analysis import show_analysis
 # from redshift import show_redshift
 # from black_hole import show_black_hole
 
-st.set_page_config(page_title = 'ODYSSEY | AstroAlgo',page_icon="page_icon.png", layout="wide", menu_items={
-        'Get Help': 'https://www.linkedin.com/company/astroalgo',
-        'Report a bug': "https://forms.gle/5hKrGPJGqs2aeHUC8",
-        'About': '''$Odyssey$ by [AstroAlgo](https://www.linkedin.com/company/astroalgo): Data-Driven Insights Into The Cosmos'''
+st.set_page_config(page_title = 'ODYSSEY',page_icon="page_icon.png", layout="wide", menu_items={
+        'Get Help': 'https://www.linkedin.com/in/maazjamshaid/',
+        'Report a bug': "https://www.linkedin.com/in/maazjamshaid/",
+        'About': '''$Odyssey$: Data-Driven Insights Into The Cosmos'''
     })
 
-st.sidebar.image("astroalgo.png", use_column_width=True)
+# st.sidebar.image("astroalgo.png", use_column_width=True)
 
 st.sidebar.markdown("---")
 
